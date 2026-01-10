a7423e663d2e1020260d474ff46c655385286923
David-Horjet <davidojetola956@gmail.com>
2026-01-10 18:46:34 +0100

    chore: addin contents

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="fixed top-0 left-0 w-full bg-[#16191d] z-50">
-      <div className="max-w-5xl mx-auto px-4 py-8 flex justify-between items-center">
+      <div className="max-w-6xl mx-auto px-4 py-8 flex justify-between items-center">
         <div className="flex items-center gap-2 cursor-pointer group">
           <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 0H4V4H0V12H4V16H12V12H16V4H12V0ZM12 4V12H4V4H12Z" fill="white"/>
@@ -27,13 +27,6 @@ const Header: React.FC = () => {
               <span className="text-primary font-medium">#</span>contacts
             </li>
           </ul>
-          
-          <div className="flex items-center gap-1 text-gray cursor-pointer hover:text-white transition-colors">
-            <span className="font-bold">EN</span>
-            <svg width="8" height="4" viewBox="0 0 8 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0 0L4 4L8 0H0Z" fill="#ABB2BF"/>
-            </svg>
-          </div>
         </nav>
 
         {/* Mobile Menu Icon */}
